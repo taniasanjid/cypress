763d2387bda13bd02be21469a16999f700b170ab
Tania Sanjid <taniasanjid9@gmail.com>
2022-10-06 19:21:29 +0600

    updated text

--- cypress/e2e/E2E.spec.js
+++ cypress/e2e/E2E.spec.js
@@ -24,7 +24,7 @@ const dashboard =new PageObject();
 
 
     it("Verify customer can view Dashboard successfully", function () {
-        dashboard.textCheck();
+      dashboard.textCheck();
     });
 
  
